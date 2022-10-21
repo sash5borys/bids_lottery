6af16f43402a49a3c30dd47c61191b3b85268473
sash5borys <sash5borys@gmail.com>
2022-10-21 11:58:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Bids Test</header>
+        <div>
+            <button>Connect</button>
+
+            
+        </div>
+    </div>
+  );
+}
+
+export default App;
